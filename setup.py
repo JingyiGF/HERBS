5f94c1c3c8617a89768327aa5ef1058970ede29e
Jingyi Guo Fuglstad <jingyi.g.fuglstad@gmail.com>
2022-06-17 21:43:32 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -1,23 +1,33 @@
 """
 @author: Jingyi GF{jingyi.g.fuglstad@gmail.com}
 """
-
-from setuptools import setup
-from herbs import __VERSION__
 import os
+from setuptools import setup, find_packages
+exec(open('herbs/version.py').read())
+
 
 # Utility function to read the README file.
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "herbs",
-    version = __VERSION__,
-    author = "Jingyi GF",
-    author_email = "jingyi.g.fuglstad@gmail.com",
+    name="herbs",
+    version=__version__,
+    author="Jingyi GF",
+    author_email="jingyi.g.fuglstad@gmail.com",
     description="",
-    keywords = "herbs",
-    url = "https://github.com/JingyiGF/HERBS",
-    packages = ['herbs'],
-    long_description = read('README.md')
+    keywords="brain atlas, histological image registration, probe coordinates",
+    url="https://github.com/JingyiGF/HERBS",
+    packages=find_packages(exclude=[]),
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/JingyiGF/HERBS/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
